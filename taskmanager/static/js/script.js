312c61342f690d06f4e4b922eaec4f73ca155594
Maya-Claveau <maya.claveau@gmail.com>
2022-05-18 14:02:06 +0000

    Add add_task function, but doesn't work yet

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
-     M.Sidenav.init(elems, options);
-  });
+    M.Sidenav.init(elems, options);
+
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select element initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+});
